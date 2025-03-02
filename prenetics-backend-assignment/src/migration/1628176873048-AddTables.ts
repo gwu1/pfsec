a7215ec5fa8ca2a425e3c29edb9e809281e96f63
Gene Wu <genehk2@gmail.com>
2025-03-02 23:40:59 +0800

    Fixed a migration - wrong type for result

--- prenetics-backend-assignment/src/migration/1628176873048-AddTables.ts
+++ prenetics-backend-assignment/src/migration/1628176873048-AddTables.ts
@@ -4,7 +4,7 @@ export class AddTables1628176873048 implements MigrationInterface {
     name = 'AddTables1628176873048'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "test"."result" ("resultid" uuid NOT NULL DEFAULT uuid_generate_v4(), "activationtime" TIMESTAMP NOT NULL DEFAULT now(), "activatedby" text, "sampleid" text NOT NULL, "resultTime" TIMESTAMP NOT NULL DEFAULT now(), "result" jsonb, "type" text NOT NULL DEFAULT 'rtpcr', "profileProfileId" uuid, CONSTRAINT "PK_89263399e8916df2433096569f0" PRIMARY KEY ("resultid"))`);
+        await queryRunner.query(`CREATE TABLE "test"."result" ("resultid" uuid NOT NULL DEFAULT uuid_generate_v4(), "activationtime" TIMESTAMP NOT NULL DEFAULT now(), "activatedby" text, "sampleid" text NOT NULL, "resultTime" TIMESTAMP NOT NULL DEFAULT now(), "result" text, "type" text NOT NULL DEFAULT 'rtpcr', "profileProfileId" uuid, CONSTRAINT "PK_89263399e8916df2433096569f0" PRIMARY KEY ("resultid"))`);
         await queryRunner.query(`CREATE INDEX "IDX_d3fca82fd5cec7fde77da14fa8" ON "test"."result" ("sampleid") `);
         await queryRunner.query(`CREATE TABLE "test"."profile" ("profileid" uuid NOT NULL DEFAULT uuid_generate_v4(), "datetime" TIMESTAMP NOT NULL DEFAULT now(), "name" text NOT NULL, "organisationOrganisationId" uuid, CONSTRAINT "PK_d5057b796d3765e4091d1b48193" PRIMARY KEY ("profileid"))`);
         await queryRunner.query(`CREATE TABLE "test"."organisation" ("organisationid" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" text NOT NULL, "datetime" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_57b8038304edebf1cb21171238c" PRIMARY KEY ("organisationid"))`);
